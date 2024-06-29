3764e334301774226eaa01cbfb30817c5ac4bbf2
Reyney91 <andrew011901@mail.ru>
2024-06-29 16:51:13 +0500

    test: testing ci checks from github

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { CssBaseline } from '@mui/material';
 import ThemeProvider from './app/providers/ThemeProvider/ThemeProvider.tsx';
 import { StoreProvider } from './app/providers/StoreProvider/index.ts';
 import { ScrollProvider } from '@/app/providers/ScrollProvider';
-//test github actions
+//test github
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <ScrollProvider>
